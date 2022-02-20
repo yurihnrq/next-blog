b8c83e4c5ee5880555252c336025ad16689c8c97
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-02-19 22:43:39 -0300

    fix: unused files removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css';
+import React from 'react';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
-}
+};
 
-export default MyApp;
+export default App;
