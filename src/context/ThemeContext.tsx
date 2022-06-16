60cd46f9b79e8ac7133faaa15de0f14edd782f18
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-06-16 15:58:31 -0300

    refactor: ThemeContext should store theme id in localStorage

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import GlobalStyle from '../styles/global';
 import { ThemeProvider, ThemeConsumer as Consumer } from 'styled-components';
 import { DarkTheme, LightTheme, Theme } from '../styles/themes';
@@ -9,13 +9,25 @@ interface IProvider {
 }
 
 const Provider: React.FC<IProvider> = ({ children }) => {
-  const [theme, setTheme] = usePersistentState<Theme>('theme', LightTheme);
+  const [themeId, setThemeId] = usePersistentState<typeof LightTheme.id>(
+    'theme',
+    LightTheme.id
+  );
+  const [theme, setTheme] = useState<Theme>(() => {
+    if (themeId === LightTheme.id) {
+      return LightTheme;
+    }
+
+    return DarkTheme;
+  });
 
   const toggleTheme = useCallback(() => {
-    setTheme(previousTheme =>
-      previousTheme.id === 'light' ? DarkTheme : LightTheme
-    );
-  }, [setTheme]);
+    setThemeId(prevThemeId => (prevThemeId === 'light' ? 'dark' : 'light'));
+  }, [setThemeId]);
+
+  useEffect(() => {
+    setTheme(() => (themeId === 'light' ? LightTheme : DarkTheme));
+  }, [themeId]);
 
   return (
     <ThemeProvider theme={{ ...theme, toggleTheme }}>
